df6796f72e7e9512cc8f342a26d20e80b25488bf
Ashwi0612 <vagu.ash0612@gmail.com>
2026-02-20 09:38:30 +0530

    Filename changed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PalindomeCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
