c90b0eef411f85a142d559517b8d9b64f10f704d
Ashwi0612 <vagu.ash0612@gmail.com>
2026-03-07 18:58:22 +0530

    Added usecase UC6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,6 @@
-import java.util.Stack;
-import java.util.Scanner;
+import java.util.*;
 
-public class UC5PalindromeCheckerApp {
+public class UC6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
@@ -9,19 +8,28 @@ public class UC5PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
         for (char c : str.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
 
-        String reversed = "";
+        boolean palindrome = true;
 
-        while (!stack.isEmpty()) {
-            reversed += stack.pop();
+        while (!queue.isEmpty()) {
+
+            char fromQueue = queue.remove();
+            char fromStack = stack.pop();
+
+            if (fromQueue != fromStack) {
+                palindrome = false;
+                break;
+            }
         }
 
-        if (str.equalsIgnoreCase(reversed))
+        if (palindrome)
             System.out.println("Palindrome");
         else
             System.out.println("Not a Palindrome");
