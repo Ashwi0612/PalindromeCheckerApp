04382f34fb2ee4f0c7cd52d401eed7e382adf1f6
Ashwi0612 <vagu.ash0612@gmail.com>
2026-03-07 18:36:06 +0530

    Added usecase UC3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
-public class PalindromeCheckerApp {
+
+public class UC3PalindroneCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String word = sc.nextLine();
-        boolean isPalindrome = true;
-        int length = word.length();
-
-        // Loop runs only till half of the string length
-        for (int i = 0; i < length / 2; i++) {
-            if (word.charAt(i) != word.charAt(length - 1 - i)) {
-                isPalindrome = false;
-                break;   // Stop checking once mismatch is found
-            }
+        String original = sc.nextLine();
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
-        if (isPalindrome) {
-            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        if (original.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
         } else {
-            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+            System.out.println("The given string is NOT a Palindrome.");
         }
-        System.out.println("Program exited successfully.");
         sc.close();
     }
 }
